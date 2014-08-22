c9700e4829f4fb73ecca0eaa612edb04e0565829
Victor Costa <vfcosta@gmail.com>
2014-08-22 14:53:11 -0300

    proposals_discussion: custom body label

--- lib/proposals_discussion_plugin/discussion.rb
+++ lib/proposals_discussion_plugin/discussion.rb
@@ -3,6 +3,10 @@ class ProposalsDiscussionPlugin::Discussion < Folder
   has_many :topics, :class_name => 'ProposalsDiscussionPlugin::Topic', :foreign_key => 'parent_id'
   has_many :proposals, :class_name => 'ProposalsDiscussionPlugin::Proposal', :through => :children, :source => :children
 
+  settings_items :custom_body_label, :type => :string, :default => _('Body')
+
+  attr_accessible :custom_body_label
+
   def self.short_description
     _("Discussion")
   end

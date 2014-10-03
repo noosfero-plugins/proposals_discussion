997946d1688c9676361e1c1c2687a19f28f5e8fd
Victor Costa <vfcosta@gmail.com>
2014-10-03 13:51:44 -0300

    proposals_discussion: proposals will be public by default

--- lib/proposals_discussion_plugin/proposal_helper.rb
+++
@@ -1,8 +0,0 @@
-module ProposalsDiscussionPlugin::ProposalHelper
-
-  def visibility_options(article, tokenized_children)
-    article.published = false if article.new_record?
-    super
-  end
-
-end

437ba5644e404f700ebd457ceb40c8fb3327df18
Victor Costa <vfcosta@gmail.com>
2014-10-06 10:16:30 -0300

    proposals_discussion: added image icon in topics

--- lib/proposals_discussion_plugin/topic_helper.rb
+++ lib/proposals_discussion_plugin/topic_helper.rb
@@ -1,8 +1,12 @@
 module ProposalsDiscussionPlugin::TopicHelper
 
   def topic_title(topic)
-    content_tag(:div, '', :class=>'topic-color', :style => "background-color: #{topic.color};") +
-      content_tag(:h2, link_to(topic.title, topic.view_url))
+    image_icon = topic.image ?  image_tag(topic.image.public_filename(:thumb), :class => 'disable-zoom') : ''
+
+    content_tag(:div, (
+      content_tag(:div, '', :class=>'topic-color', :style => "background-color: #{topic.color};") +
+        content_tag(:h2, link_to(image_icon + content_tag(:span, topic.title), topic.view_url))
+    ), :class => 'topic-title')
   end
 
 end

32e6b1fef12caf0eb674f516a39ac88ea418bd82
Evandro Junior <evandrojr@gmail.com>
2015-04-06 17:57:48 -0300

    fix layout issue https://gitlab.com/participa/noosfero/issues/415

--- test/functional/content_viewer_controller_test.rb
+++ test/functional/content_viewer_controller_test.rb
@@ -4,13 +4,13 @@ class ContentViewerControllerTest < ActionController::TestCase
 
   def setup
     @profile = fast_create(Community)
-
+    @person = fast_create(Person)
     @discussion = ProposalsDiscussionPlugin::Discussion.create!(:name => 'test', :profile => @profile)
     @topic = ProposalsDiscussionPlugin::Topic.create!(:name => 'test', :profile => @profile, :parent => @discussion)
     @proposal = ProposalsDiscussionPlugin::Proposal.create!(:name => 'test', :profile => @profile, :parent => @topic, :abstract => "Abstract", :body => "Proposal Body")
   end
 
-  attr_reader :profile, :proposal, :topic, :discussion
+  attr_reader :profile, :proposal, :topic, :discussion, :person
 
   should 'display custom proposal page' do
     get :view_page, proposal.url
@@ -31,5 +31,20 @@ class ContentViewerControllerTest < ActionController::TestCase
     get :view_page, discussion.url
     assert_template 'content_viewer/discussion'
   end
+
+  should 'do not load class proposals-discussion-infinite-scroll when listing topics' do
+    discussion.allow_topics = true
+    proposals = fast_create(ProposalsDiscussionPlugin::Proposal, :name => 'proposal title', :abstract => 'proposal abstract', :profile_id => profile.id, :parent_id => topic.id, :author_id => person.id)
+    get :view_page, discussion.url
+    assert_no_tag :tag => "div", :attributes => { :class => "proposals-discussion-infinite-scroll" }
+  end
+
+  should 'load class proposals-discussion-infinite-scroll when listing topics' do
+    discussion.allow_topics = true
+    proposals = fast_create(ProposalsDiscussionPlugin::Proposal, :name => 'proposal title', :abstract => 'proposal abstract', :profile_id => profile.id, :parent_id => topic.id, :author_id => person.id)
+    get :view_page, topic.url
+    assert_tag :tag => "div", :attributes => { :class => "proposals-discussion-infinite-scroll" }
+  end
+
 
 end

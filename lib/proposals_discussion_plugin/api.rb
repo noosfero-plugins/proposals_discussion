6f252a32edffca0ad71daeee1affa630be2a8c37
Victor Costa <vfcosta@gmail.com>
2015-05-08 10:49:00 -0300

    Change ranking algorithm

--- lib/proposals_discussion_plugin/api.rb
+++ lib/proposals_discussion_plugin/api.rb
@@ -4,12 +4,15 @@ class ProposalsDiscussionPlugin::API < Grape::API
 
     get ':id/ranking' do
       article = find_article(environment.articles, params[:id])
-      Rails.cache.fetch("#{article.cache_key}/proposals_ranking", expires_in: 1.hours) do
-        proposals = article.proposals.map do |proposal|
-          effective_support = (proposal.votes_for - proposal.votes_against)/proposal.hits.to_f
-          effective_participation = (proposal.votes_for + proposal.votes_against)/proposal.hits.to_f
+      Rails.cache.fetch("#{article.cache_key}/proposals_ranking", expires_in: 10.minutes) do
+        max_hits = article.proposals.maximum(:hits)
+        min_hits = article.proposals.minimum(:hits)
 
-          {:id => proposal.id, :abstract => proposal.abstract, :votes_for => proposal.votes_for, :votes_against => proposal.votes_against, :hits => proposal.hits, :effective_support => effective_support, :effective_participation => effective_participation}
+        proposals = article.proposals.map do |proposal|
+          w = [(proposal.hits - max_hits).abs, (proposal.hits - min_hits).abs].max.to_f
+          effective_support = (proposal.votes_for - proposal.votes_against)/w
+
+          {:id => proposal.id, :abstract => proposal.abstract, :votes_for => proposal.votes_for, :votes_against => proposal.votes_against, :hits => proposal.hits, :effective_support => effective_support}
         end
         proposals = proposals.sort_by { |p| p[:effective_support] }.reverse
         {:proposals => proposals, :updated_at => DateTime.now}

6d5f6b634a4a492bc5c40cf69372d63f41969c75
Victor Costa <vfcosta@gmail.com>
2015-06-03 17:42:50 -0300

    Add api method to create proposal task

--- test/unit/api_test.rb
+++ test/unit/api_test.rb
@@ -34,4 +34,15 @@ class APITest <  ActiveSupport::TestCase
     assert_equal [proposal2.id, proposal3.id, proposal1.id], json['proposals'].map {|p| p['id']}
   end
 
+  should 'suggest article children' do
+    discussion = fast_create(ProposalsDiscussionPlugin::Discussion, :profile_id => user.person.id)
+    topic = fast_create(ProposalsDiscussionPlugin::Topic, :profile_id => user.person.id, :parent_id => discussion.id)
+    params[:article] = {:name => "Proposal name", :body => "Proposal body"}
+    assert_difference "ProposalsDiscussionPlugin::ProposalTask.count" do
+      post "/api/v1/proposals_discussion_plugin/#{topic.id}/propose?#{params.to_query}"
+    end
+    json = JSON.parse(last_response.body)
+    assert json['success']
+  end
+
 end

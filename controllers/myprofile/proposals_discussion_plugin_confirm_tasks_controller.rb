2e0b07f0b7684047ba8086e2e2158dfb0a92ba2d
Ábner Silva de Oliveira <abner.oliveira@serpro.gov.br>
2015-06-30 17:04:56 -0300

    added feature to allow confirm proposals individually

---
+++ controllers/myprofile/proposals_discussion_plugin_confirm_tasks_controller.rb
@@ -0,0 +1,43 @@
+class ProposalsDiscussionPluginConfirmTasksController < MyProfileController
+  protect :perform_task, :profile, :only => [:approve_proposal, :reprove_proposal]
+
+  def approve_proposal
+    result = {}
+    if request.post?
+      result = process_decision(params, :finish)
+    end
+    render json: result
+  end
+
+  def reprove_proposal
+    result = {}
+    if request.post?
+      result = process_decision(params, :cancel)
+    end
+    render json: result
+  end
+
+private
+
+  def process_decision(params, decision)
+    result = {
+      success: false,
+      message: _('Error flagging proposal. Please, contact the system admin')
+    }
+    if params[:task_id] and request.post?
+      begin
+        task = profile.find_in_all_tasks(params[:task_id])
+        task.tag_list = params[:tag_list]
+        task.article_parent_id = params[:article_parent_id]
+        task.email_template_id = params[:email_template_id]
+        task.send(decision, current_person)
+        result = {:success =>  true }
+      rescue Exception => ex
+        message = "#{task.title} (#{task.requestor ? task.requestor.name : task.author_name})" if task
+        message = "#{message} #{ex.message}"
+        result[:message] += ". #{message}"
+      end
+      return result
+    end
+  end
+end
--- controllers/myprofile/proposals_discussion_plugin_evaluate_tasks_controller.rb
+++ controllers/myprofile/proposals_discussion_plugin_evaluate_tasks_controller.rb
@@ -1,13 +1,14 @@
 class ProposalsDiscussionPluginEvaluateTasksController < MyProfileController
+  protect :view_tasks, :profile, :only => [:approve_proposal, :reprove_proposal]
 
-  protect :view_tasks, :profile, :only => [:flag_approve_proposal, :flag_reject_proposal]
-
-  def flag_approve_proposal
+  def approve_proposal
+    result = {}
     if request.post? && params[:task_id]
       result = {
         success: false,
         message: _('Error flagging proposal. Please, contact the system admin')
       }
+
 
       task = Task.to(profile).find_by_id params[:task_id]
       save = task.flag_accept_proposal(current_person)
@@ -20,7 +21,8 @@ class ProposalsDiscussionPluginEvaluateTasksController < MyProfileController
     render json: result
   end
 
-  def flag_reprove_proposal
+  def reprove_proposal
+    result = {}
     if request.post? && params[:task_id]
       result = {
         success: false,

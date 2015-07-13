58defcbc50caa2bd1c2f970b03bdf1df2cb57df8
Michel Felipe de Oliveira Ferreira <michel.ferreira@serpro.gov.br>
2015-07-13 16:16:36 -0300

    Fix the save_categories action permission in ProposalTasksController

--- controllers/myprofile/proposals_discussion_plugin_tasks_controller.rb
+++ controllers/myprofile/proposals_discussion_plugin_tasks_controller.rb
@@ -1,7 +1,11 @@
 class ProposalsDiscussionPluginTasksController < TasksController
 
+  protect [:perform_task, :view_tasks], :profile, :only => [:index, :save_tags, :search_tags, :save_categories]
+  protect :perform_task, :profile, :except => [:index, :save_tags, :search_tags, :save_categories]
+
   def index
-    @email_templates = profile.email_templates.find_all_by_template_type(:task_rejection)
+    @rejection_email_templates = profile.email_templates.find_all_by_template_type(:task_rejection)
+    @acceptance_email_templates = profile.email_templates.find_all_by_template_type(:task_acceptance)
 
     @filter_type = params[:filter_type].presence
     @filter_text = params[:filter_text].presence

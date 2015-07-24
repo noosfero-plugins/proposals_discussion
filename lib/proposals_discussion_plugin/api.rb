5e126d2f1c48c59f761f33d7d7535d798aee8f1b
Michel Felipe de Oliveira Ferreira <michel.ferreira@serpro.gov.br>
2015-07-24 18:27:47 -0300

    Added improvement performance in api on endpoint :id/propose.

--- lib/proposals_discussion_plugin/api.rb
+++ lib/proposals_discussion_plugin/api.rb
@@ -1,6 +1,6 @@
 class ProposalsDiscussionPlugin::API < Grape::API
+  use ProposalsDiscussionPlugin::ApiNewRelicInstrumenter
 
-  
   resource :proposals_discussion_plugin do
 
     paginate per_page: 10, max_per_page: 20
@@ -26,11 +26,11 @@ class ProposalsDiscussionPlugin::API < Grape::API
     post ':id/propose' do
       sanitize_params_hash(params)
 
-      parent_article = environment.articles.find(params[:id])
+      parent_article = environment.articles.includes(:profile).find(params[:id])
 
       proposal_task = ProposalsDiscussionPlugin::ProposalTask.new
       proposal_task.article = params[:article]
-      proposal_task.article_parent_id = parent_article.id
+      proposal_task.article_parent = parent_article
       proposal_task.target = parent_article.profile
       proposal_task.requestor = current_person
 

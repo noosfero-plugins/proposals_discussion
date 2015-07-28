44e370a0e631a1740e1b8d19c56e35df32fdf918
Victor Costa <vfcosta@gmail.com>
2015-07-28 17:35:04 -0300

    Revert "Disable cache on ranking"
    This reverts commit 34e1b1bda56186e7f1632fe72f9679692d7f43a1.

--- lib/proposals_discussion_plugin/api.rb
+++ lib/proposals_discussion_plugin/api.rb
@@ -5,7 +5,7 @@ class ProposalsDiscussionPlugin::API < Grape::API
     paginate per_page: 10, max_per_page: 20
     get ':id/ranking' do
       article = find_article(environment.articles, params[:id])
-      #ranking = Rails.cache.fetch("#{article.cache_key}/proposals_ranking", expires_in: 30.minutes) do
+      ranking = Rails.cache.fetch("#{article.cache_key}/proposals_ranking", expires_in: 10.minutes) do
         max_hits = article.proposals.maximum(:hits)
         min_hits = article.proposals.minimum(:hits)
 
@@ -16,8 +16,8 @@ class ProposalsDiscussionPlugin::API < Grape::API
           {:id => proposal.id, :abstract => proposal.abstract, :votes_for => proposal.votes_for, :votes_against => proposal.votes_against, :hits => proposal.hits, :effective_support => effective_support}
         end
         proposals = proposals.sort_by { |p| p[:effective_support] }.reverse
-        ranking = {:proposals => proposals, :updated_at => DateTime.now}
-      #end
+        {:proposals => proposals, :updated_at => DateTime.now}
+      end
       ranking[:proposals] = paginate ranking[:proposals]
       ranking
     end

bf59537c3fc4026757975c9644163445568dcc88
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-09-24 21:34:05 -0300

    expose ranking correctly

--- lib/ext/entities.rb
+++ lib/ext/entities.rb
@@ -2,12 +2,19 @@ module Noosfero
   module API
     module Entities
 
-      class ArticleBase < Entity
-        expose :ranking_position do |article, options|
-          article.kind_of?(ProposalsDiscussionPlugin::Proposal) && article.ranking_item.present? ? article.ranking_item.position : nil
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      module ClassMethods
+        class ArticleBase < Entity
+          expose :ranking_position do |article, options|
+            article.kind_of?(ProposalsDiscussionPlugin::Proposal) && article.ranking_item.present? ? article.ranking_item.position : nil
+          end
         end
       end
 
     end
   end
+
 end

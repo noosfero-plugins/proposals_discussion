5aa7a5fa14d9ba12b0588696449a5a89c60c1092
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-10-01 13:41:49 -0300

    expose ranking and amount_of_children correctly

--- lib/ext/entities.rb
+++ lib/ext/entities.rb
@@ -3,25 +3,15 @@ module Noosfero
   module API
     module Entities
 
-      #FIXME make test
-      class ArticleBase < Entity
+      class Article < ArticleBase
+#FIXME Leandro changed the method to model
+#        expose :ranking_position do |article, options|
+#          article.kind_of?(ProposalsDiscussionPlugin::Proposal) && article.ranking_item.present? ? article.ranking_item.position : nil
+#        end
         expose :ranking_position
         #FIXME see why children counter cache is not working
         expose :amount_of_children do |article, options|
           article.children.count
-        end
-
-      end
-
-      def self.included(base)
-        base.extend(ClassMethods)
-      end
-
-      module ClassMethods
-        class ArticleBase < Entity
-          expose :ranking_position do |article, options|
-            article.kind_of?(ProposalsDiscussionPlugin::Proposal) && article.ranking_item.present? ? article.ranking_item.position : nil
-          end
         end
       end
 
